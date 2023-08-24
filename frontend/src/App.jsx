d81cea37ed51e889fba4f486d0128f2bd905ccba
h1ze <burgov@mail.ru>
2023-08-24 11:15:06 +0400

    refactor: add route dependency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header/Header"
 import Input from "./components/input/Input"
 
 function App() {
   return (
     <>
+      <Header/>
       <Input placeholder='Email' />
       <Input placeholder='Введите пароль' />
     </>
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Logo from '../Logo/Logo';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Logo/>
+    </header>
+  )
+}
+
+export default Header
