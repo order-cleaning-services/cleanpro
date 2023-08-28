514af34dddfe40ce09691d7cbf3cb1dd4fdaeb7b
Alexandr Sharganov <allinshelter@gmail.com>
2023-08-28 10:57:23 +0400

    add  price model to admin.py

--- backend/cleanpro/price/admin.py
+++ backend/cleanpro/price/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Price, Measure
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'title',
+        'description',
+        'price',
+    )
+
+
+@admin.register(Measure)
+class MeasureAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'title',
+        'short_name',
+    )
