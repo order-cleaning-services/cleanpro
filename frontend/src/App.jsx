b9353439e62e9133ef2eacc3de55ea2b9e4d562d
tiron <ole_g92@bk.ru>
2023-08-30 17:54:47 +0300

    materials section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header/Header"
+import Materials from "./components/Materials/Materials"
 import Input from "./components/input/Input"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Header/>
       <Input placeholder='Email' />
       <Input placeholder='Введите пароль' />
+      <Materials />
     </>
   )
 }
