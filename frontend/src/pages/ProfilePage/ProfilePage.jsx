b6672c420f3048265a950c80737ae6bba27f9f60
h1ze <burgov@mail.ru>
2023-09-09 17:42:19 +0400

    feat: create profile form structure

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,11 @@
+import ProfileForm from '../../components/ProfileForm/ProfileForm'
+import './ProfilePage.scss'
+
 export default function Profile() {
   return (
-    <>
-      <h1>страница Profile</h1>
-    </>
+    <div className="profile">
+      <h1 className="profile__title">Профиль</h1>
+      <ProfileForm />
+    </div>
   )
 }
