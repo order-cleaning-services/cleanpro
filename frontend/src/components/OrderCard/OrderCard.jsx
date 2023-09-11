cbd9bb5889fd7b85ba4f075280568e31e7672137
h1ze <burgov@mail.ru>
2023-09-12 03:36:23 +0400

    feat: add basic OrderCard logic

--- frontend/src/components/OrderCard/OrderCard.jsx
+++ frontend/src/components/OrderCard/OrderCard.jsx
@@ -1,9 +1,14 @@
 import './OrderCard.scss'
-import date from '../../images/date.svg'
-import address from '../../images/address.svg'
+import calendar from '../../images/date.svg'
+import geo from '../../images/address.svg'
 import user from '../../images/profile.svg'
 
 const OrderCard = () => {
+  const CLEANERS = ['Климова Ольга', 'Плотников Евгений']
+  const DATE = 'Ср, 20.09.2023'
+  const TIME = '12:00-14:00'
+  const ADDRESS = 'г. Москва, ул Дмитриевского, д.1, кв 5, подъезд 3, этаж 8'
+
   return (
     <div className="card">
       <div className="card__header">
@@ -17,16 +22,24 @@ const OrderCard = () => {
       <div className="card__content">
         <ul className="card__info-list">
           <li className="card__info-line">
-            <img src={date} alt="Иконка календаря" />
-            <p className="card__date-time">
-              Ср, 20.09.2023 <span className="card__breaker">&#183;</span> 12:00-14:0
+            <img className="card__info-line-image" src={calendar} alt="Иконка календаря" />
+            <p className="card__info-line-text">
+              {DATE} <span className="card__breaker">&#183;</span> {TIME}
             </p>
           </li>
           <li className="card__info-line">
-            <img src={address} alt="Иконка геопозиции" />
+            <img className="card__info-line-image" src={geo} alt="Иконка геопозиции" />
+            <p className="card__info-line-text">{ADDRESS}</p>
           </li>
           <li className="card__info-line">
-            <img src={user} alt="Иконка геопозиции" />
+            <img className="card__info-line-image" src={user} alt="Иконка геопозиции" />
+            <div className="card__info-line-cleaners">
+              {CLEANERS.map(cleaner => (
+                <p className="card__info-line-text" key={cleaner}>
+                  {cleaner}
+                </p>
+              ))}
+            </div>
           </li>
         </ul>
       </div>
