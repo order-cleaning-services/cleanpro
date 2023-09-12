7e15f93b0ba50829fe90373412052843e9069dee
h1ze <burgov@mail.ru>
2023-09-12 04:29:47 +0400

    feat: add OrderCard buttons

--- frontend/src/components/OrderCard/OrderCard.jsx
+++ frontend/src/components/OrderCard/OrderCard.jsx
@@ -2,12 +2,21 @@ import './OrderCard.scss'
 import calendar from '../../images/date.svg'
 import geo from '../../images/address.svg'
 import user from '../../images/profile.svg'
+import up from '../../images/chevron-up.svg'
+import down from '../../images/chevron-down.svg'
+import { useState } from 'react'
 
 const OrderCard = () => {
+  // Переменные из констант нужно будет переписать в аргументы, получаемые на рендер компонентом
+
   const CLEANERS = ['Климова Ольга', 'Плотников Евгений']
   const DATE = 'Ср, 20.09.2023'
   const TIME = '12:00-14:00'
   const ADDRESS = 'г. Москва, ул Дмитриевского, д.1, кв 5, подъезд 3, этаж 8'
+
+  const [isDetailed, setIsDetailed] = useState(false)
+
+  const toggleInfo = () => setIsDetailed(!isDetailed)
 
   return (
     <div className="card">
@@ -42,6 +51,23 @@ const OrderCard = () => {
             </div>
           </li>
         </ul>
+        <div className="card__control">
+          {isDetailed ? (
+            <button className="card__control-btn card__control-btn_more" onClick={toggleInfo}>
+              <img src={up} alt="Стрелка, направленная вверх" />
+              Скрыть
+            </button>
+          ) : (
+            <button className="card__control-btn card__control-btn_more" onClick={toggleInfo}>
+              <img src={down} alt="Стрелка, направленная вниз" />
+              Посмотреть подробнее
+            </button>
+          )}
+          <div className="card__control-buttons">
+            <button className="card__control-btn">Перенести</button>
+            <button className="card__control-btn">Отменить</button>
+          </div>
+        </div>
       </div>
     </div>
   )
