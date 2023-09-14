600f9301a4f1a7ee41963ede9df1e873ef80d55b
ElPastel <gromova.victoria9@gmail.com>
2023-09-14 15:46:31 +0400

    delete styles folder

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,12 +1,12 @@
-import TransferModal from '../../components/Modal/TransferModal/TransferModal'
+// import TransferModal from '../../components/Modal/TransferModal/TransferModal'
 // import CancelModal from './../../components/Modal/CancelModal/CancelModal'
-// import ReviewModal from "../../components/Modal/ReviewModal/ReviewModal";
+// import ReviewModal from '../../components/Modal/ReviewModal/ReviewModal'
 
 export default function Profile() {
   return (
     <>
       {/* <h1>страница Profile</h1> */}
-      <TransferModal />
+      {/* <TransferModal /> */}
       {/* <CancelModal /> */}
       {/* <ReviewModal /> */}
     </>
