7b764fc668ea030230043a21c582c36247035053
Kirill Svidunovich <Altair21817@gmail.com>
2023-09-21 12:00:51 +0300

    Update IsOwnerOrReadOnly

--- backend/cleanpro/api/permissions.py
+++ backend/cleanpro/api/permissions.py
@@ -2,6 +2,11 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Предоставляет доступ:
+        - на чтение: всем;
+        - на запись: автору объекта или администратору.
+    """
 
     def has_permission(self, request, view):
         return (
@@ -10,16 +15,15 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.user == obj.user or
-            # Добавлен "is_stuff", так как superuser != администратор сайта.
-            # TODO: уточнить роли is_staff и is_superuser
-            request.user.is_staff or
-            request.user.is_superuser
-        )
+        return request.user == obj.user or request.user.is_staff
 
 
 class IsOwner(permissions.BasePermission):
+    """
+    Предоставляет доступ:
+        - на чтение - автору объекта;
+        - на запись - автору объекта.
+    """
 
     def has_permission(self, request, view):
         return request.user.is_authenticated
