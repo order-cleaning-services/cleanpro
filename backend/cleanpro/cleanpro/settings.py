775db99d26744ef56267a459fda8167bf0e45e8e
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-03 15:45:27 +0300

    Fix imports style

--- backend/cleanpro/cleanpro/settings.py
+++ backend/cleanpro/cleanpro/settings.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
 from corsheaders.defaults import default_headers
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
