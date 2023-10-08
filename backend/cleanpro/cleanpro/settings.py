f52201de9987af2a89981278e7d3cfea4143bd46
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-08 12:50:54 +0300

    Refactor settings

--- backend/cleanpro/cleanpro/settings.py
+++ backend/cleanpro/cleanpro/settings.py
@@ -8,13 +8,32 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(os.path.join(BASE_DIR, '.env'), verbose=True)
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+
+"""App settings."""
+
 
 DEBUG = True
 
-# TODO архинебезопасно! Для прода вписать допустимые данные. Лучше через
-# переменные окружения, чтобы скрыть их от злоумышленников.
-ALLOWED_HOSTS = ['*']
+CLEANPRO_HOST = os.getenv('HOST_YANDEX_MAPS', None)
+
+CLEANPRO_YA_MAPS_ID = os.getenv('HOST_YANDEX_MAPS', None)
+
+
+"""Django core settings."""
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,18 +56,60 @@ INSTALLED_APPS = [
     'price.apps.PriceConfig',
 ]
 
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 ROOT_URLCONF = 'cleanpro.urls'
+
+WSGI_APPLICATION = 'cleanpro.wsgi.application'
+
+
+"""Email settings."""
+
+
+DEFAULT_FROM_EMAIL = 'cleanpronew2023@gmail.com'
+
+EMAIL_BACKEND = (
+    'django.core.mail.backends.console.EmailBackend' if DEBUG else
+    'django.core.mail.backends.smtp.EmailBackend')
+
+EMAIL_HOST: str = os.getenv('EMAIL_HOST')
+
+EMAIL_PORT: int = int(os.getenv('EMAIL_PORT'))
+
+EMAIL_HOST_USER: str = os.getenv('EMAIL_HOST_USER')
+
+EMAIL_HOST_PASSWORD: str = os.getenv('EMAIL_HOST_PASSWORD')
+
+EMAIL_USE_TLS: bool = bool(os.getenv('EMAIL_USE_TLS'))
+
+EMAIL_USE_SSL: bool = bool(os.getenv('EMAIL_USE_SSL'))
+
+EMAIL_SSL_CERTFILE: str = os.getenv('EMAIL_SSL_CERTFILE')
+
+EMAIL_SSL_KEYFILE: str = os.getenv('EMAIL_SSL_KEYFILE')
+
+EMAIL_TIMEOUT: int = int(os.getenv('EMAIL_TIMEOUT'))
+
+
+"""Media and templates."""
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = 'media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = 'static/'
 
 TEMPLATES = [
     {
@@ -66,18 +127,39 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cleanpro.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
+"""Models data."""
+
+
+ADDITIONAL_CS = 'additional'
+
+ADMIN = 'admin'
+
+ACCOUNT_EMAIL_REQUIRED = True
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+
+ACCOUNT_USERNAME_REQUIRED = False
+
+AUTH_USER_MODEL = 'users.User'
+
+USER = 'user'
+
+
+"""Regional settings."""
+
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+"""Security settings."""
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,88 +185,37 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'ru-RU'
+# TODO архинебезопасно! Для прода вписать допустимые данные. Лучше через
+# переменные окружения, чтобы скрыть их от злоумышленников.
+ALLOWED_HOSTS = ['*']
 
-TIME_ZONE = 'UTC'
+CORS_ALLOW_CREDENTIALS = True
 
-USE_I18N = True
+CORS_ALLOW_HEADERS = [
+    *default_headers,
+    "access-control-allow-credentials",
+]
 
-USE_TZ = True
-
-STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'users.User'
-
-ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-
-ACCOUNT_USERNAME_REQUIRED = False
-
-ACCOUNT_EMAIL_REQUIRED = True
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-}
-
-USER = 'user'
-
-ADMIN = 'admin'
-
-ADDITIONAL_CS = 'additional'
-
-DEFAULT_FROM_EMAIL = 'cleanpronew2023@gmail.com'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# TODO: это для фронтов, чтобы в локальной сети был доступ к бэку.
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1',
 ]
-
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOW_HEADERS = (
-    *default_headers,
-    "access-control-allow-credentials",
-)
-
-# TODO: адекватно разделить код на смысловые блоки. Частично вынести в core.
-# Допускается не делать core, а складировать все здесь. Но навести порядок.
-"""Email backend data"""
-
-EMAIL_HOST: str = os.getenv('EMAIL_HOST')
-EMAIL_PORT: int = int(os.getenv('EMAIL_PORT'))
-EMAIL_HOST_USER: str = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD: str = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS: bool = bool(os.getenv('EMAIL_USE_TLS'))
-EMAIL_USE_SSL: bool = bool(os.getenv('EMAIL_USE_SSL'))
-EMAIL_SSL_CERTFILE: str = os.getenv('EMAIL_SSL_CERTFILE')
-# TODO: проверить SSL_KEYFILE
-EMAIL_SSL_KEYFILE: str = os.getenv('EMAIL_SSL_KEYFILE')
-EMAIL_TIMEOUT: int = int(os.getenv('EMAIL_TIMEOUT'))
-
-# TODO: при выключении DEBUG будет ошибка, так как SMTP у нас не арендован
-# и не подключен.
-EMAIL_BACKEND = (
-    'django.core.mail.backends.console.EmailBackend' if DEBUG else
-    'django.core.mail.backends.smtp.EmailBackend')
 
 DJOSER = {
     'SERIALIZERS': {
         'user': 'api.serializers.CustomUserSerializer',
     }
 }
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+SECRET_KEY = os.getenv('SECRET_KEY')
