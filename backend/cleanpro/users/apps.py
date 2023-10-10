0f6c9407463d7d14527570679c183446116c9285
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-10 21:01:59 +0300

    Register post_save_receiver

--- backend/cleanpro/users/apps.py
+++ backend/cleanpro/users/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class UsersConfig(AppConfig):
     name = 'users'
     verbose_name = 'Пользователи'
+
+    def ready(self) -> None:
+        from .signals import post_save_receiver
+
+        post_save.connect(
+            receiver=post_save_receiver,
+            dispatch_uid='update_cached_reviews',
+        )
+        return super().ready()
