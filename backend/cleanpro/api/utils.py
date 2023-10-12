406e389c1c988da6b50b9b0b8f65dc71960fb83e
Kirill Svidunovich <Altair21817@gmail.com>
2023-10-13 01:22:29 +0300

    Add get_available_cleaners

--- backend/cleanpro/api/utils.py
+++ backend/cleanpro/api/utils.py
@@ -2,13 +2,16 @@
 Вспомогательные функции приложения Api.
 """
 
+from datetime import date, datetime, time, timedelta
 import random
 import string
 
 from django.core import mail
+from django.db.models import Q, QuerySet
 
 from cleanpro.app_data import DEFAULT_FROM_EMAIL, EMAIL_CODE_LENGTH
-from users.models import Address
+from service.models import Order
+from users.models import Address, User
 
 
 def generate_code():
@@ -51,3 +54,50 @@ def send_mail(subject: str, message: str, to: tuple[str]) -> None:
             connection=conn
         ).send(fail_silently=False)
     return
+
+
+def get_available_cleaners(
+        cleaning_date: date,
+        cleaning_time: time,
+        total_time: int) -> QuerySet:
+    """
+    Получает данные об уборке (дата, время, продолжительность) и возвращает
+    список пользователей, которые доступны для ее выполнения.
+    """
+    cleaning_end_time: time = (
+        datetime.combine(cleaning_date, cleaning_time) +
+        timedelta(minutes=total_time)
+    ).time()
+    overlapping_orders: QuerySet = Order.objects.filter(
+        cleaning_date=cleaning_date,
+    ).filter(
+        Q(
+            cleaning_time__gte=cleaning_time,
+            cleaning_time__lt=cleaning_end_time,
+        ) |
+        Q(
+            cleaning_time_end__gt=cleaning_time,
+            cleaning_time_end__lte=cleaning_end_time,
+        ) |
+        Q(
+            cleaning_time__lte=cleaning_time,
+            cleaning_time_end__gte=cleaning_end_time,
+        ) |
+        Q(
+            cleaning_time__gte=cleaning_time,
+            cleaning_time_end__lte=cleaning_end_time,
+        )
+    )
+    all_cleaners: QuerySet = User.objects.filter(
+        is_cleaner=True,
+    ).filter(
+        Q(on_vacation_from__isnull=True) |
+        Q(on_vacation_from__gt=cleaning_date) |
+        Q(on_vacation_to__lt=cleaning_date)
+    )
+    busy_cleaners: QuerySet = overlapping_orders.values_list(
+        'cleaner',
+        flat=True,
+    )
+    available_cleaners: QuerySet = all_cleaners.exclude(pk__in=busy_cleaners)
+    return available_cleaners
